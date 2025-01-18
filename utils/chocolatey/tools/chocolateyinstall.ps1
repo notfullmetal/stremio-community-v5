667995aa97e55a54066ab88d0fdaee58b95e6e20
Zarg <62082797+Zaarrg@users.noreply.github.com>
2025-01-18 14:03:22 +0100

    Version Bump 5.0.12

--- utils/chocolatey/tools/chocolateyinstall.ps1
+++ utils/chocolatey/tools/chocolateyinstall.ps1
@@ -11,12 +11,12 @@ $packageArgs = @{
 
 
 if ([Environment]::Is64BitOperatingSystem) {
-    $packageArgs['url']          = 'https://github.com/Zaarrg/stremio-desktop-v5/releases/download/5.0.0-beta.11/Stremio.5.0.11-x64.exe'
-    $packageArgs['checksum']     = '9333f379ce0ce04a9743f033b39ce6c4a00a4bba231009ebba0454532c7c10aa'
+    $packageArgs['url']          = 'https://github.com/Zaarrg/stremio-desktop-v5/releases/download/5.0.0-beta.12/Stremio.5.0.12-x64.exe'
+    $packageArgs['checksum']     = 'e7c045800d502874b8e083427aa81d316625c658fa804e67156dddd4f03ea6ed'
     $packageArgs['checksumType'] = 'sha256'
 } else {
-    $packageArgs['url']          = 'https://github.com/Zaarrg/stremio-desktop-v5/releases/download/5.0.0-beta.11/Stremio.5.0.11-x86.exe'
-    $packageArgs['checksum']     = '12535d930dc8c7ec260ab4ab83e189862bae5b848d6d68d9dc00167e4c434013'
+    $packageArgs['url']          = 'https://github.com/Zaarrg/stremio-desktop-v5/releases/download/5.0.0-beta.12/Stremio.5.0.12-x86.exe'
+    $packageArgs['checksum']     = '715ec5def305a019849b3a5e402398b53b5ed242f065266384f4428ed4c39818'
     $packageArgs['checksumType'] = 'sha256'
 }
 
